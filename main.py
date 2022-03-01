102bba2643847251b77c9e6933b053b7543fa263
Joel R. Frank <56587789+jrfrank87@users.noreply.github.com>
2022-02-28 23:11:08 -0400

    Update
    Trying to keep the binary from terminating after running os.system command

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 
-menuChoice = input("=============\n= Main menu =\n=============\n[1]Enable Super Admin Account\n[2]Disable Super Admin Account\n[3]Add New User Account\n[4]List All Users\n[5]Delete a user account\n")
+menuChoice = input("=============\n= Main menu =\n=============\n[1]Enable Super Admin Account\n[2]Disable Super Admin Account\n[3]List All Users\n[4]Add New User Account\n[5]Delete a user account\n")
 if menuChoice == "1":
     os.system("Net user administrator /active:yes")
+    print("Super admin account enabled.")
 if menuChoice == "2":
     os.system("Net user administrator /active:no")
+    print("Super admin account disabled.")
 if menuChoice == "3":
     os.system("Net user")
 if menuChoice =="4":
